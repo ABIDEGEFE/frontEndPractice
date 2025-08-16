a77cae40aab476cbfad9b0c873f7df3921287f47
Abinet <agonaferdegefe@gmail.com>
2025-08-16 15:39:49 +0300

    how javaScript interact with html and css

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(4 != "4");
+document.querySelector('.heading1').innerHTML = "TBET"
+
+document.querySelector(".heading1").style.backgroundColor = "red"
+
+
+function changeColorButton() {
+   document.querySelector("h1").classList.toggle("hidden")
+}
+
+function sideBarAction () {
+    document.querySelector('.sidebar').classList.toggle("remove");
+}
